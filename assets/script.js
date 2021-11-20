d7fc059fd1f235e5c943b17f4bcfa62c180b40a4
KatieG <katieshake27@gmail.com>
2021-11-20 01:59:14 -0500

    working on javascript

--- assets/script.js
+++ assets/script.js
@@ -18,56 +18,57 @@ var currentDay = moment().format("ddd, MMM, Do, YYYY")
 $("#currentDay").text(currentDay);
 console.log(currentDay);
 
-var tasksInputEl = document.querySelector('#user-input')
+var tasksInputEl = document.querySelector('#user-input');
+var taskList = document.querySelector('.textarea');
+var tasks = [];
+
+function showTasks() {
+    tasksInputEl.innerHTML = '';
+    tasksInputEl.textContent = tasks.length;
+    for (var i = 0; i < tasks.length; i++) {
+        var task = tasks[i];
+
+        var p = document.createElement("p");
+        p.textContent = task;
+
+    }
+}
 
 var saveBtn = $(".saveBtn")
 saveBtn.on("click", function () {
     console.log("Event saved")
 
     function saveTask() {
+        console.log('saveTask')
         var taskValue = tasksInputEl.value.trim();
         console.log(taskValue)
         if (taskValue) {
-            tasks = JSON.parse(localStorage.getItem('taskValue'));
+            tasks = JSON.parse(localStorage.getItem('taskValue', taskValue));
             if (!tasks) {
                 tasks = [];
-                console.log(taskValue)
-                tasks.unshift(taskValue);
-                localStorage.setItem('taskValue', JSON.stringify(taskValue));
             }
+            console.log(taskValue)
+
+            localStorage.setItem('taskValue', JSON.stringify(taskValue));
+            console.log(tasks)
         }
+        return;
     }
-    function init() {
-        var savedTasks = JSON.parse(localStorage.getItem('taskValue'));
-        console.log(savedTasks)
-        if (savedTasks !== null) {
-            taskValue = savedTasks;
-            console.log(savedTasks);
-            for (var i = 0; i < savedTasks.length; i++) {
-                saveTask(savedTasks);
-                console.log(savedTasks);
-            }
-        }
-    
-}
-init();
+    saveTask();
 
 })
-    
-    
+function init() {
+    var savedTasks = JSON.parse(localStorage.getItem('taskValue'));
+    console.log(savedTasks)
+    if (savedTasks !== null) {
+        taskValue = savedTasks;
+        taskValue = [];
+        console.log(savedTasks);
+    }
 
-    // var formSearch = function (event) {
-    //     console.log('formSearch');
-    //     event.preventDefault();
-    //     var task = taskContent.value.trim();
-    //     if (task) {
-    //         saveTask();
-    //         userInputEl.textContent = '';
-    //         userInputEl.textContent = 'Loading...';
-    //         taskContent.value = '';
-    //     }
-    //     pastSearch(task);
-    //     console.log(task);
+}
+showTasks();
+init();
 
 
 
