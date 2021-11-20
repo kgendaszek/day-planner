2f546273aff7c260f09e515ef5693ff914c5590b
KatieG <katieshake27@gmail.com>
2021-11-19 22:38:29 -0500

    working on local storage

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// console.log("script.js is running")
+console.log("script.js is running")
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
@@ -18,24 +18,58 @@ var currentDay = moment().format("ddd, MMM, Do, YYYY")
 $("#currentDay").text(currentDay);
 console.log(currentDay);
 
-//save button
+var tasksInputEl = document.querySelector('#user-input')
+
 var saveBtn = $(".saveBtn")
 saveBtn.on("click", function () {
     console.log("Event saved")
+
+    function saveTask() {
+        var taskValue = tasksInputEl.value.trim();
+        console.log(taskValue)
+        if (taskValue) {
+            tasks = JSON.parse(localStorage.getItem('taskValue'));
+            if (!tasks) {
+                tasks = [];
+                console.log(taskValue)
+                tasks.unshift(taskValue);
+                localStorage.setItem('taskValue', JSON.stringify(taskValue));
+            }
+        }
+    }
+    function init() {
+        var savedTasks = JSON.parse(localStorage.getItem('taskValue'));
+        console.log(savedTasks)
+        if (savedTasks !== null) {
+            taskValue = savedTasks;
+            console.log(savedTasks);
+            for (var i = 0; i < savedTasks.length; i++) {
+                saveTask(savedTasks);
+                console.log(savedTasks);
+            }
+        }
+    
+}
+init();
+
 })
-function handleProjectFormSubmit(event) {
-        event.preventDefault();
-}
+    
+    
 
-//save user input to local storage
-var userInput = $("#description")
-function createItem() {
-    localStorage.setItem('userInput', 'text');
-}
-createItem()
+    // var formSearch = function (event) {
+    //     console.log('formSearch');
+    //     event.preventDefault();
+    //     var task = taskContent.value.trim();
+    //     if (task) {
+    //         saveTask();
+    //         userInputEl.textContent = '';
+    //         userInputEl.textContent = 'Loading...';
+    //         taskContent.value = '';
+    //     }
+    //     pastSearch(task);
+    //     console.log(task);
 
-function getValue() {
-    return localStorage.getItem('userInput');
-}
-console.log(getValue());
+
+
+
 
